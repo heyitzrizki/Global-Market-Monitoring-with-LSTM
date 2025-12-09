21f208b14a1918b6e2fc11d48ef98d08fa4efac2
RizkiAnwar <135109764+heyitzrizki@users.noreply.github.com>
2025-12-09 12:30:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ st.set_page_config(
 )
 
 DATA_PATH = "data/global_market_master.csv"
-MODEL_PATH = "model/lstm_model.h5"
-SCALER_PATH = "model/scaler.pkl"
+MODEL_PATH = "models/lstm_model.h5"
+SCALER_PATH = "models/scaler.pkl"
 
 
 @st.cache_data
